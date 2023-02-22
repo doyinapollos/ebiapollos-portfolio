3e4956720e9fde7dd2a4484e08f6d2f637efb5ed
ebiapollos <doyinapollos@gmail.com>
2023-02-22 14:58:23 +0100

    project setup 2

---
+++ src/helpers/url.js
@@ -0,0 +1,10 @@
+const AuthRoutes = {
+    dashboard: "/dashboard",
+};
+
+/** Non Authenticated URL Routes */
+const NonAuthRoutes = {
+    home: "/",
+};
+
+export { AuthRoutes, NonAuthRoutes };
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="w-screen h-screen bg-slate-500 ">
+      <nav className="w-full h-[100px] bg-red-500 flex justify-center items-center ">
+        <p className="">menu</p>
+      </nav>
+    </div>
+  );
+}
+
+export default Home;
